5ae3cda2c66483ebff9f070735163269f277c5ee
easiedata <easiedata@easiedatas-MacBook-Air.local>
2021-02-25 18:12:40 -0300

    working on easiedata components

---
+++ src/components/easie_echart/easie_echart.vue
@@ -0,0 +1,66 @@
+<template>
+  <div ref="my_echart" :style="c_style"></div>
+</template>
+
+<script>
+  import * as echarts from 'echarts';
+  export default {
+    name: 'easie_echart',
+    props: {
+      echarts_json: { required: true, type: Object },
+      upd_chart_size: { default: 0 },
+      height: { default: '300px' },
+      geo_json_map:{required:false}
+    },
+    data() {
+      return {
+        myChart: {}
+      };
+    },
+    mounted() {
+      this.myChart = echarts.init(this.$refs.my_echart, 'easie-custom-1');
+
+      if(this.geo_json_map){
+        echarts.registerMap('map', this.geo_json_map, {})
+      }
+
+      this.myChart.setOption(this.echarts_json);
+      let v_self = this;
+      this.myChart.on('click', function (params) {
+        if(params.seriesType == 'pie'){
+          let group = params.seriesName;
+          let data = params.name;
+          v_self.$emit('clicked_series', {group: group, data: data});
+          return;
+        }
+        if(params.componentType == 'series'){
+          let group = params.name;
+          let data = params.seriesName;
+          v_self.$emit('clicked_series', {group: group, data: data});
+          return;
+        }
+      });
+
+
+      $(window).on('resize', function() {
+        v_self.myChart.resize();
+      });
+    },
+    watch: {
+      upd_chart_size: function() {
+        this.myChart.resize();
+      },
+      height(){
+        this.myChart.resize();
+      }
+    },
+    computed: {
+      c_style: function() {
+        return `height:${this.height}; width: 100%;`;
+      }
+    }
+  };
+</script>
+
+<style>
+</style>
--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import * as easieComponents from './components';
 import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faAngleUp, faAngleDown, faTimes, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faAngleUp, faAngleDown, faChevronDown, faSave, faWrench, faChartLine, faChartBar, faChartPie, faComment, faSuperscript } from '@fortawesome/free-solid-svg-icons';
 import VTooltip from 'v-tooltip';
 
 
 Vue.use(VTooltip);
+
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
-library.add(faAngleDown, faAngleUp, faTimes, faChevronDown);
+library.add(faAngleDown, faAngleUp, faChevronDown, faSave, faWrench, faChartLine, faChartBar, faChartPie, faComment, faSuperscript);
 
 let easiedata = Vue => {
     Object.values(easieComponents).forEach((easieComponent) => {
@@ -20,9 +21,14 @@ let easiedata = Vue => {
 
 export default easiedata;
 
+export { default as easieChart } from './components/easie_chart/easie_chart.vue';
 export { default as easieCheckbox } from './components/easie_checkbox/easie_checkbox.vue';
+export { default as easieDefaultMeta } from './components/easie_default_meta/easie_default_meta.vue';
 export { default as easieFormInput } from './components/easie_form_input/easie_form_input.vue';
 export { default as easieHr } from './components/easie_hr/easie_hr.vue';
+export { default as easieIndicator } from './components/easie_indicator/easie_indicator.vue';
+export { default as easieItemTools } from './components/easie_item_tools/easie_item_tools.vue';
+export { default as easieMap } from './components/easie_map/easie_map.vue';
 export { default as easieModal } from './components/easie_modal/easie_modal.vue';
 export { default as easieRadio } from './components/easie_radio/easie_radio.vue';
 export { default as easieRadioList } from './components/easie_radio_list/easie_radio_list.vue';
@@ -31,6 +37,6 @@ export { default as easieSketchColor } from './components/easie_sketch_color/eas
 export { default as easieSwitch } from './components/easie_switch/easie_switch.vue';
 export { default as easieTab } from './components/easie_tabs/easie_tab.vue';
 export { default as easieTopTabs } from './components/easie_tabs/easie_top_tabs.vue';
-export { default as easieTextArea } from './components/easie_textarea/easie_textarea.vue';
+export { default as easieTextarea } from './components/easie_textarea/easie_textarea.vue';
 export { default as tooltipLabel } from './components/tooltip_label/tooltip_label.vue';
 
